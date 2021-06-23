cbbfc2cc75b3bf179cbce6faacf7ec3b106fba77
dusk-darknight <78079763+dusk-darknight@users.noreply.github.com>
2021-06-23 15:15:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var backgroundImg;
+
+var snow = [];
+
+function preload() {
+    backgroundImg = loadImage("snow3.jpg");
+}
+
+function setup(){
+    var canvas = createCanvas(displayWidth,displayHeight);
+    engine = Engine.create();
+    world = engine.world;
+
+    ground = new Ground(displayWidth/2,displayHeight,displayWidth,20);
+    
+  
+    
+    
+
+}
+
+function draw(){
+   
+    imageMode(CENTER)
+    image(backgroundImg,displayWidth/2,displayHeight/2,displayWidth,displayHeight)
+    Engine.update(engine);
+
+    if(frameCount%10===0){
+        snow.push(new Snow(random(100, displayWidth), 10));
+      
+      }
+    
+     for (var j = 0; j < snow.length; j++) {
+      
+        snow[j].display();
+      }
+   
+    ground.display();
+}
